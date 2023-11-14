664a2e40739153aad7ab0632a73a0ea0b07927fd
1devndogs <vsalgado.a@gmail.com>
2023-11-14 17:22:04 -0300

    chore: update sold out banner

--- src/app/countdown.tsx
+++ src/app/countdown.tsx
@@ -23,7 +23,21 @@ const Countdown: React.FC<CountdownProps> = ({ targetTime }) => {
   const minutes = timeDifference.minutes();
   const seconds = timeDifference.seconds();
 
-  return <div className="pl-2">Corsair is Sold Out</div>;
+  return (
+    <div className="pl-2 flex items-center text">
+      <p className="mr-4">Corsair Insignia is Sold Out</p>
+      <a
+        href="https://opensea.io/collection/4762-corsair-membership"
+        target="https://opensea.io/collection/4762-corsair-membership"
+      >
+        <img
+          width={24}
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADYAAAA2CAYAAACMRWrdAAAAAXNSR0IArs4c6QAAAK5JREFUaIHtmkEKwCAMBGPx/1+2x16qsCW23TBzrahDYEnVCAAAuGhjjN1rzBZoyiStScPjkEYbgZgbiLmxSkU1zbLiNSUty1YMMTcQc6PH/jRT50/pLctWDDE3EHMj8w/6k15xRtmKIeYGYm486RVnqaX2imr6SfssWzHE3EDMjSfnin/jNl3LVgwxNxBzoy++7b5VUeFcMQIxPxBzY5o0L7zYSYE76Cog5kZZsRMvLRtc5z2dXgAAAABJRU5ErkJggg=="
+          alt="opensea"
+        ></img>
+      </a>
+    </div>
+  );
 };
 
 const addLeadingZero = (value: any) => (value < 10 ? `0${value}` : value);
