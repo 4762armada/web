ccf1bc3103037878d1350d2300ccf75624760e3b
1devndogs <vsalgado.a@gmail.com>
2023-11-14 23:36:22 -0300

    chore: soon message on btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,24 @@ import Sidebar from "./sidebar";
 export default function Home() {
   const [fontLoaded, setFontLoaded] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [buttonText1, setButtonText1] = useState("Address Checker");
+  const [buttonText2, setButtonText2] = useState("Mint Punk Armada");
+
+  const handleMouseEnter1 = () => {
+    setButtonText1("Soon");
+  };
+
+  const handleMouseLeave1 = () => {
+    setButtonText1("Address Checker");
+  };
+
+  const handleMouseEnter2 = () => {
+    setButtonText2("Soon");
+  };
+
+  const handleMouseLeave2 = () => {
+    setButtonText2("Mint Punk Armada");
+  };
 
   useEffect(() => {
     const fontLink = document.createElement("link");
@@ -32,47 +50,44 @@ export default function Home() {
         }`}
         onClick={() => setIsSidebarOpen(false)}
       ></div>
-      <div className="header fixed h-20 left-0 top-0 z-10 w-full flex items-center justify-between ">
-        {/* Contenedor para el texto, centrado */}
+      <div className="header fixed h-20 left-0 top-0 z-10 w-full flex items-center justify-between pr-2 pl-2">
         <Countdown targetTime={targetTime} />
-
-        {/* Contenedor para el botón, al final */}
-        <img
-          onClick={() => setIsSidebarOpen(true)}
-          src="/menu.png"
-          width={24}
-          style={{ filter: "invert(100%)" }}
-          className="mr-4"
-        ></img>
+        <span
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="border-l-4 border-white pl-2 text-blue-600"
+        >
+          {"Menu"}
+        </span>
       </div>
       <div className="grid text-center lg:max-w-5xl lg:w-full lg:grid-cols-2 pt-60 pb-60 mb-40">
         <a
-          href="https://checker.4762armada.com"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          onMouseEnter={handleMouseEnter1}
+          onMouseLeave={handleMouseLeave1}
         >
           <button
             disabled
-            className="eightbit-btn"
+            className="eightbit-btn mb-4"
             type="button"
             style={{ color: "#538f40", width: "20.3em" }}
+ 
           >
-            <p>Address Checker</p>
+            <p>{buttonText1}</p>
           </button>
         </a>
 
         <a
-          href="https://mint.4762armada.com/"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          onMouseEnter={handleMouseEnter2}
+          onMouseLeave={handleMouseLeave2}
         >
           <button
-            className="eightbit-btn"
+            className="eightbit-btn mb-4"
             type="button"
             disabled
             style={{ color: "#538f40", width: "20.3em" }}
           >
-            <p>MINT Punk Armada</p>
+            <p>{buttonText2}</p>
           </button>
         </a>
       </div>
