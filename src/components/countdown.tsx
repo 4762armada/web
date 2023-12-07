ae616feaa8015abe781c7d61db701e860223a48f
1devndogs <vsalgado.a@gmail.com>
2023-12-06 22:03:22 -0300

    chore: update mint date

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,12 +1,9 @@
 const Countdown: React.FC = ({}) => {
   return (
     <div className="marquee w-full whitespace-nowrap pl-2 flex items-center text">
-      <div className="track">
-        - WE MINTING PUNK ARMADA ON DECEMBER 3RD -
-      </div>
+      <div className="track">- WE MINTING PUNK ARMADA ON DECEMBER 7TH -</div>
     </div>
   );
 };
-
 
 export default Countdown;
